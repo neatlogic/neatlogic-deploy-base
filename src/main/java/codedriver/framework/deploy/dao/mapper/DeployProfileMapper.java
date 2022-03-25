acd1977e6fa8ec8e1114f0679875b994f0ce51d2
longrf <techsure901>
2022-03-25 15:08:57 +0800

    --story=1006013 --user=龙儒锋 【运维自动化】工具Profile https://www.tapd.cn/54247054/s/1215886

--- src/main/java/codedriver/framework/deploy/dao/mapper/DeployProfileMapper.java
+++ src/main/java/codedriver/framework/deploy/dao/mapper/DeployProfileMapper.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 public interface DeployProfileMapper {
 
-     int searchDeployProfileCount(DeployProfileVo profileVo);
+    int searchDeployProfileCount(DeployProfileVo profileVo);
 
     int checkProfileIsExists(Long id);
 

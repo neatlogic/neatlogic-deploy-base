b193cd7d79d96d3f6f07aedc2d390566eccb653e
longrf <a5739119>
2022-04-15 17:20:45 +0800

    --story=1006147 --user=龙儒锋 自动发布-场景定义 https://www.tapd.cn/54247054/s/1221314

---
+++ src/main/java/codedriver/framework/deploy/dto/scene/DeploySceneVo.java
@@ -0,0 +1,47 @@
+package codedriver.framework.deploy.dto.scene;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BaseEditorVo;
+import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
+
+/**
+ * @author longrf
+ * @date 2022/4/15 12:06 下午
+ */
+public class DeploySceneVo extends BaseEditorVo {
+
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "名称", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "描述", type = ApiParamType.STRING)
+    private String description;
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

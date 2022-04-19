e93d46a27f52b73ea5d98fba26d18330c0d8c42d
longrf <a5739119>
2022-04-19 18:02:07 +0800

    --story=1006146 --user=龙儒锋 自动发布-全局参数 https://www.tapd.cn/54247054/s/1222063

---
+++ src/main/java/codedriver/framework/deploy/dto/param/DeployGlobalParamVo.java
@@ -0,0 +1,56 @@
+package codedriver.framework.deploy.dto.param;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BaseEditorVo;
+import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
+
+/**
+ * @author longrf
+ * @date 2022/4/18 6:59 下午
+ */
+public class DeployGlobalParamVo extends BaseEditorVo {
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "参数名", type = ApiParamType.STRING)
+    private String name;
+    @EntityField(name = "显示名", type = ApiParamType.STRING)
+    private String displayName;
+    @EntityField(name = "描述", type = ApiParamType.STRING)
+    private String description;
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

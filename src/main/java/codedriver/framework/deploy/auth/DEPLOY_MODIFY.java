e53cf093dc5c8123a8d73c10d1b3fdee3bba9fad
longrf <a5739119>
2022-04-20 10:58:54 +0800

    --story=1006147 --user=龙儒锋 自动发布-场景定义 https://www.tapd.cn/54247054/s/1222162

---
+++ src/main/java/codedriver/framework/deploy/auth/DEPLOY_MODIFY.java
@@ -0,0 +1,38 @@
+package codedriver.framework.deploy.auth;
+
+import codedriver.framework.auth.core.AuthBase;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author longrf
+ * @date 2022/4/15 5:09 下午
+ */
+public class DEPLOY_MODIFY extends AuthBase {
+
+    @Override
+    public String getAuthDisplayName() {
+        return "自动发布管理员权限";
+    }
+
+    @Override
+    public String getAuthIntroduction() {
+        return "自动发布全局参数管理、场景定义";
+    }
+
+    @Override
+    public String getAuthGroup() {
+        return "deploy";
+    }
+
+    @Override
+    public Integer getSort() {
+        return 4;
+    }
+
+    @Override
+    public List<Class<? extends AuthBase>> getIncludeAuths() {
+        return Collections.singletonList(DEPLOY_BASE.class);
+    }
+}

9b1e5dc4f86901b74ae4bcaf0469b54d47e8d3c9
longrf <a5739119>
2022-04-28 14:26:49 +0800

    --story=1006221 --user=龙儒锋 自动化-sql文件执行 阶段的作业详情页面修改 https://www.tapd.cn/54247054/s/1224275

---
+++ src/main/java/codedriver/framework/deploy/crossover/IDeploySqlCrossoverMapper.java
@@ -0,0 +1,22 @@
+package codedriver.framework.deploy.crossover;
+
+import codedriver.framework.crossover.ICrossoverService;
+import codedriver.framework.deploy.dto.sql.DeploySqlDetailVo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author longrf
+ * @date 2022/4/28 2:10 下午
+ */
+public interface IDeploySqlCrossoverMapper extends ICrossoverService {
+
+    DeploySqlDetailVo getAutoexecJobIdByDeploySqlDetailVo(DeploySqlDetailVo paramDeploySqlVo);
+
+    List<DeploySqlDetailVo> getJobDeploySqlDetailList(@Param("sqlFileDetailVoList") List<DeploySqlDetailVo> sqlFileDetailVoList);
+
+    List<DeploySqlDetailVo> getAllJobDeploySqlDetailList(DeploySqlDetailVo deployVersionSql);
+
+    void updateJobDeploySqlDetailIsDeleteAndStatusAndMd5AndLcdById(@Param("status") String status, @Param("md5") String md5, @Param("id") Long id);
+}

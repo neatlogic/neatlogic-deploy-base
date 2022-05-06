a3e1b9067739091ddf5dc4086a685a92e00ff47f
longrf <a5739119>
2022-05-06 14:43:17 +0800

    --story=1006221 --user=龙儒锋 自动化-sql文件执行 阶段的作业详情页面修改 https://www.tapd.cn/54247054/s/1224275

--- src/main/java/codedriver/framework/deploy/crossover/IDeploySqlCrossoverMapper.java
+++ src/main/java/codedriver/framework/deploy/crossover/IDeploySqlCrossoverMapper.java
@@ -1,8 +1,9 @@
 package codedriver.framework.deploy.crossover;
 
-import codedriver.framework.deploy.dto.sql.DeploySqlVo;
+import codedriver.framework.autoexec.dto.job.AutoexecJobPhaseNodeVo;
 import codedriver.framework.crossover.ICrossoverService;
 import codedriver.framework.deploy.dto.sql.DeploySqlDetailVo;
+import codedriver.framework.deploy.dto.sql.DeploySqlVo;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -19,11 +20,15 @@ public interface IDeploySqlCrossoverMapper extends ICrossoverService {
 
     List<DeploySqlDetailVo> getAllDeploySqlDetailList(DeploySqlDetailVo deployVersionSql);
 
-    void updateDeploySqlDetailIsDeleteAndStatusAndMd5AndLcdById(@Param("status") String status, @Param("md5") String md5, @Param("id") Long id);
+    void updateDeploySqlDetailIsDeleteAndStatusAndMd5ById(@Param("status") String status, @Param("md5") String md5, @Param("id") Long id);
 
     void updateDeploySqlIsDeleteByIdList(@Param("idList") List<Long> idList);
 
     void insertDeploySqlDetail(DeploySqlDetailVo paramDeploySqlVo);
 
     void insertDeploySql(DeploySqlVo jobId);
+
+    int searchDeploySqlCount(AutoexecJobPhaseNodeVo jobPhaseNodeVo);
+
+    List<DeploySqlDetailVo> searchDeploySql(AutoexecJobPhaseNodeVo jobPhaseNodeVo);
 }

1ebd8c9a309aa33d7d3cad3752343dd45935c0a8
lvzk <lvzk@techsure.com.cn>
2022-05-23 18:22:52 +0800

    --task=1006357 --user=lvzk 应用配置-tree 相关接口 https://www.tapd.cn/54247054/s/1230298

---
+++ src/main/java/codedriver/framework/deploy/dto/app/DeployAppConfigResourceVo.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c)  2022 TechSure Co.,Ltd.  All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.deploy.dto.app;
+
+import codedriver.framework.cmdb.dto.resourcecenter.ResourceVo;
+
+public class DeployAppConfigResourceVo extends ResourceVo {
+    private static final long serialVersionUID = 8079670497813410203L;
+
+    private Integer isFavorite;
+
+    public Integer getIsFavorite() {
+        return isFavorite;
+    }
+
+    public void setIsFavorite(Integer isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+}

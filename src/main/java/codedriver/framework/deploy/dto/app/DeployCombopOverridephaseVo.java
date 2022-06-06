06d477c9a3649ac0b5375fd022fd044f9ec32da9
linbq@techsure.com.cn <Besiter789>
2022-06-06 11:35:06 +0800

    --task=1006425 --user=林邦泉 后端-应用配置-流水线 https://www.tapd.cn/54247054/s/1233495

---
+++ src/main/java/codedriver/framework/deploy/dto/app/DeployCombopOverridephaseVo.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.deploy.dto.app;
+
+public class DeployCombopOverridephaseVo {
+
+    private Long id;
+    private Long appSystemId;
+    private Long moduleId;
+    private Long envId;
+    private Long combopId;
+    private Long phaseId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAppSystemId() {
+        return appSystemId;
+    }
+
+    public void setAppSystemId(Long appSystemId) {
+        this.appSystemId = appSystemId;
+    }
+
+    public Long getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(Long moduleId) {
+        this.moduleId = moduleId;
+    }
+
+    public Long getEnvId() {
+        return envId;
+    }
+
+    public void setEnvId(Long envId) {
+        this.envId = envId;
+    }
+
+    public Long getCombopId() {
+        return combopId;
+    }
+
+    public void setCombopId(Long combopId) {
+        this.combopId = combopId;
+    }
+
+    public Long getPhaseId() {
+        return phaseId;
+    }
+
+    public void setPhaseId(Long phaseId) {
+        this.phaseId = phaseId;
+    }
+}

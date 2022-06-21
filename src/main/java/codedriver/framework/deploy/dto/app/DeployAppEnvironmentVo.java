c6fa0b56c9bd492e44914a0a2a912c7adf98fdcc
longrf <a5739119>
2022-06-21 18:06:45 +0800

    --story=1006473 --user=龙儒锋 应用配置-应用模块树 https://www.tapd.cn/54247054/s/1236345

--- src/main/java/codedriver/framework/deploy/dto/app/DeployAppEnvironmentVo.java
+++ src/main/java/codedriver/framework/deploy/dto/app/DeployAppEnvironmentVo.java
@@ -1,8 +1,11 @@
 package codedriver.framework.deploy.dto.app;
 
 import codedriver.framework.cmdb.dto.resourcecenter.entity.AppEnvironmentVo;
+import codedriver.framework.cmdb.dto.resourcecenter.entity.AppModuleVo;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.EntityField;
+
+import java.util.List;
 
 /**
  * @author longrf
@@ -10,8 +13,10 @@ import codedriver.framework.restful.annotation.EntityField;
  */
 public class DeployAppEnvironmentVo extends AppEnvironmentVo {
 
-    @EntityField(name = "环境id", type = ApiParamType.INTEGER)
+    @EntityField(name = "是否可编辑", type = ApiParamType.INTEGER)
     private Integer isEdit;
+    @EntityField(name = "应用模块列表", type = ApiParamType.JSONARRAY)
+    private List<AppModuleVo> appModuleList;
 
     public Integer getIsEdit() {
         return isEdit;
@@ -19,5 +24,13 @@ public class DeployAppEnvironmentVo extends AppEnvironmentVo {
 
     public void setIsEdit(Integer isEdit) {
         this.isEdit = isEdit;
+    }
+
+    public List<AppModuleVo> getAppModuleList() {
+        return appModuleList;
+    }
+
+    public void setAppModuleList(List<AppModuleVo> appModuleList) {
+        this.appModuleList = appModuleList;
     }
 }

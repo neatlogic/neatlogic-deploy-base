24d7f6d5d9f5876c57d6f6c1bee81e1d65fbd36a
赖文韬 <laiwt@techsure.com.cn>
2022-06-30 10:21:49 +0800

    --story=1006390 --user=赖文韬 版本中心-文件管理 https://www.tapd.cn/54247054/s/1238559

--- src/main/java/codedriver/framework/deploy/exception/MoveFileFailedException.java
+++ src/main/java/codedriver/framework/deploy/exception/MoveFileFailedException.java
@@ -11,6 +11,6 @@ public class MoveFileFailedException extends ApiRuntimeException {
     private static final long serialVersionUID = -7615529238650830358L;
 
     public MoveFileFailedException(String error) {
-        super("文件复制或重命名失败，错误：" + error);
+        super("文件移动或重命名失败，错误：" + error);
     }
 }

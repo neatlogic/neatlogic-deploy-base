0685b44e102142bc4f1bbac94da8fc275e4fd221
longrf <a5739119>
2022-07-04 18:06:04 +0800

    --story=1006324 --user=龙儒锋 应用配置-基础信息&&授权 https://www.tapd.cn/54247054/s/1237201

---
+++ src/main/java/codedriver/framework/deploy/dto/app/DeployAppConfigEnvDBConfigVo.java
@@ -0,0 +1,120 @@
+package codedriver.framework.deploy.dto.app;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+/**
+ * @author longrf
+ * @date 2022/7/1 3:29 下午
+ */
+public class DeployAppConfigEnvDBConfigVo {
+
+    private static final long serialVersionUID = -1948079786425118588L;
+    @EntityField(name = "应用id", type = ApiParamType.LONG)
+    private Long appSystemId;
+    @EntityField(name = "模块id", type = ApiParamType.LONG)
+    private Long appModuleId;
+    @EntityField(name = "环境id", type = ApiParamType.LONG)
+    private Long envId;
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "数据库别名", type = ApiParamType.STRING)
+    private String DBAlias;
+    @EntityField(name = "数据库资产id", type = ApiParamType.LONG)
+    private Long DBResourceId;
+    @EntityField(name = "执行用户列表", type = ApiParamType.JSONARRAY)
+    private List<DeployAppConfigEnvDBConfigAccountVo> accountList;
+    @EntityField(name = "高级设置", type = ApiParamType.JSONOBJECT)
+    private JSONObject config;
+    @JSONField(serialize = false)
+    private String configStr;
+
+    public Long getAppSystemId() {
+        return appSystemId;
+    }
+
+    public void setAppSystemId(Long appSystemId) {
+        this.appSystemId = appSystemId;
+    }
+
+    public Long getAppModuleId() {
+        return appModuleId;
+    }
+
+    public void setAppModuleId(Long appModuleId) {
+        this.appModuleId = appModuleId;
+    }
+
+    public Long getEnvId() {
+        return envId;
+    }
+
+    public void setEnvId(Long envId) {
+        this.envId = envId;
+    }
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDBAlias() {
+        return DBAlias;
+    }
+
+    public void setDBAlias(String DBAlias) {
+        this.DBAlias = DBAlias;
+    }
+
+    public Long getDBResourceId() {
+        return DBResourceId;
+    }
+
+    public void setDBResourceId(Long DBResourceId) {
+        this.DBResourceId = DBResourceId;
+    }
+
+    public List<DeployAppConfigEnvDBConfigAccountVo> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(List<DeployAppConfigEnvDBConfigAccountVo> accountList) {
+        this.accountList = accountList;
+    }
+
+
+    public JSONObject getConfig() {
+        if (MapUtils.isEmpty(config) && StringUtils.isNotBlank(configStr)) {
+            config = JSONObject.parseObject(configStr);
+        }
+        return config;
+    }
+
+    public void setConfig(JSONObject config) {
+        this.config = config;
+    }
+
+    public String getConfigStr() {
+        if (StringUtils.isEmpty(configStr) && MapUtils.isNotEmpty(config)) {
+            configStr = config.toJSONString();
+        }
+        return configStr;
+    }
+
+    public void setConfigStr(String configStr) {
+        this.configStr = configStr;
+    }
+}

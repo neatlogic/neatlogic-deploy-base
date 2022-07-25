e112ce5baaebac1854bca66a5e3ef3126134b104
longrf <a5739119>
2022-07-25 11:56:46 +0800

    修复获取创建发布作业初始化流水线信息

--- src/main/java/codedriver/framework/deploy/dto/sql/DeploySqlDetailVo.java
+++ src/main/java/codedriver/framework/deploy/dto/sql/DeploySqlDetailVo.java
@@ -75,6 +75,8 @@ public class DeploySqlDetailVo extends BaseEditorVo {
     private String costTime;
     @EntityField(name = "sql排序", type = ApiParamType.INTEGER)
     private Integer sort;
+    @EntityField(name = "是否改动", type = ApiParamType.INTEGER)
+    private Integer isModified = 0;
 
     @EntityField(name = "sql文件名列表", type = ApiParamType.JSONARRAY)
     private List<String> sqlFiles;
@@ -93,6 +95,7 @@ public class DeploySqlDetailVo extends BaseEditorVo {
         this.nodeName=(paramObj.getString("nodeName"));
         this.resourceId=(paramObj.getLong("resourceId"));
         this.runnerId=(paramObj.getLong("runnerId"));
+        this.isModified=(paramObj.getInteger("isModified"));
     }
 
     public DeploySqlDetailVo(Long sysId, Long envId, Long moduleId, String version, String sqlFile, Long jobId, String phaseName) {
@@ -342,5 +345,13 @@ public class DeploySqlDetailVo extends BaseEditorVo {
 
     public void setSort(Integer sort) {
         this.sort = sort;
+    }
+
+    public Integer getIsModified() {
+        return isModified;
+    }
+
+    public void setIsModified(Integer isModified) {
+        this.isModified = isModified;
     }
 }

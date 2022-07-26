41412b3b99d23985255802bd4af018cfe6cca876
陈其炜 <chenqw@techsure.com.cn>
2022-07-26 10:15:31 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/deploy/dto/DeployJobVo.java
+++ src/main/java/codedriver/framework/deploy/dto/DeployJobVo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c)  2022 TechSure Co.,Ltd.  All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -39,6 +39,7 @@ public class DeployJobVo {
     private String configStr;
     @JSONField(serialize = false)
     private String configHash;
+
     public DeployJobVo(){
 
     }
---
+++ src/main/java/codedriver/framework/deploy/dto/batchjob/LaneGroupVo.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.deploy.dto.batchjob;
+
+import codedriver.framework.autoexec.constvalue.JobStatus;
+import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
+
+import java.util.List;
+
+public class LaneGroupVo {
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "是否需要等待", type = ApiParamType.INTEGER)
+    private Integer needWait;
+    @EntityField(name = "状态", type = ApiParamType.ENUM, member = JobStatus.class)
+    private String status;
+    @EntityField(name = "作业列表", type = ApiParamType.JSONARRAY)
+    private List<AutoexecJobVo> jobList;
+
+    @EntityField(name = "排序", type = ApiParamType.INTEGER)
+    private Integer sort;
+
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<AutoexecJobVo> getJobList() {
+        return jobList;
+    }
+
+    public void setJobList(List<AutoexecJobVo> jobList) {
+        this.jobList = jobList;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public Integer getNeedWait() {
+        return needWait;
+    }
+
+    public void setNeedWait(Integer needWait) {
+        this.needWait = needWait;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}

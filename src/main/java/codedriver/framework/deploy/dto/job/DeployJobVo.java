1ac46cd98cd1205222d8dcbf8e0d72543ce8c16f
陈其炜 <chenqw@techsure.com.cn>
2022-07-26 15:50:10 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/deploy/dto/job/DeployJobVo.java
+++ src/main/java/codedriver/framework/deploy/dto/job/DeployJobVo.java
@@ -3,8 +3,9 @@
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
-package codedriver.framework.deploy.dto;
+package codedriver.framework.deploy.dto.job;
 
+import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.deploy.dto.app.DeployPipelineConfigVo;
 import codedriver.framework.restful.annotation.EntityField;
@@ -15,16 +16,26 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DeployJobVo {
+public class DeployJobVo extends AutoexecJobVo {
     @EntityField(name = "id", type = ApiParamType.LONG)
     private Long id;
     @EntityField(name = "应用资产id", type = ApiParamType.LONG)
     private Long appSystemId;
+
+    @EntityField(name = "应用资产名称", type = ApiParamType.STRING)
+    private String appSystemName;
     @EntityField(name = "系统模块id", type = ApiParamType.LONG)
     private Long appModuleId;
+
+    @EntityField(name = "系统模块名称", type = ApiParamType.STRING)
+    private String appModuleName;
     @EntityField(name = "环境资产id", type = ApiParamType.LONG)
     private Long envId;
+    @EntityField(name = "环境资产名称", type = ApiParamType.STRING)
+    private String envName;
     @EntityField(name = "版本", type = ApiParamType.STRING)
     private String version;
     @EntityField(name = "编译号", type = ApiParamType.INTEGER)
@@ -34,14 +45,20 @@ public class DeployJobVo {
     @EntityField(name = "编译|构造的runner_id", type = ApiParamType.LONG)
     private Long runnerMapId;
     @EntityField(name = "流水线配置信息", type = ApiParamType.JSONOBJECT)
-    private DeployPipelineConfigVo config;
+    private DeployPipelineConfigVo pipeLineConfig;
     @JSONField(serialize = false)
-    private String configStr;
+    private String pipeLineConfigStr;
     @JSONField(serialize = false)
     private String configHash;
+    @EntityField(name = "泳道列表")
+    private List<LaneVo> laneList;
 
-    public DeployJobVo(){
 
+    public DeployJobVo() {
+        List<String> sourceList = new ArrayList<>();
+        sourceList.add("deploy");
+        sourceList.add("batchdeploy");
+        this.setSourceList(sourceList);
     }
 
     public DeployJobVo(JSONObject jsonObj) {
@@ -50,6 +67,42 @@ public class DeployJobVo {
         envId = jsonObj.getLong("envId");
         version = jsonObj.getString("version");
         buildNo = jsonObj.getInteger("buildNo");
+        List<String> sourceList = new ArrayList<>();
+        sourceList.add("deploy");
+        sourceList.add("batchdeploy");
+        this.setSourceList(sourceList);
+    }
+
+    public String getAppSystemName() {
+        return appSystemName;
+    }
+
+    public void setAppSystemName(String appSystemName) {
+        this.appSystemName = appSystemName;
+    }
+
+    public String getAppModuleName() {
+        return appModuleName;
+    }
+
+    public void setAppModuleName(String appModuleName) {
+        this.appModuleName = appModuleName;
+    }
+
+    public String getEnvName() {
+        return envName;
+    }
+
+    public void setEnvName(String envName) {
+        this.envName = envName;
+    }
+
+    public List<LaneVo> getLaneList() {
+        return laneList;
+    }
+
+    public void setLaneList(List<LaneVo> laneList) {
+        this.laneList = laneList;
     }
 
     public DeployJobVo(Long _jobId, Long _runnerMapId) {
@@ -63,6 +116,7 @@ public class DeployJobVo {
         }
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -115,19 +169,19 @@ public class DeployJobVo {
         this.runnerMapId = runnerMapId;
     }
 
-    public void setConfig(DeployPipelineConfigVo config) {
-        this.config = config;
+    public void setPipeLineConfig(DeployPipelineConfigVo pipeLineConfig) {
+        this.pipeLineConfig = pipeLineConfig;
     }
 
-    public String getConfigStr() {
-        if (configStr == null && config != null) {
-            configStr = JSONObject.toJSONString(config);
+    public String getPipeLineConfigStr() {
+        if (pipeLineConfigStr == null && pipeLineConfig != null) {
+            pipeLineConfigStr = JSONObject.toJSONString(pipeLineConfig);
         }
-        return configStr;
+        return pipeLineConfigStr;
     }
 
-    public void setConfigStr(String configStr) {
-        this.configStr = configStr;
+    public void setPipeLineConfigStr(String pipeLineConfigStr) {
+        this.pipeLineConfigStr = pipeLineConfigStr;
     }
 
     public Integer getBuildNo() {
@@ -138,13 +192,13 @@ public class DeployJobVo {
         this.buildNo = buildNo;
     }
 
-    public DeployPipelineConfigVo getConfig() {
-        return config;
+    public DeployPipelineConfigVo getPipeLineConfig() {
+        return pipeLineConfig;
     }
 
     public String getConfigHash() {
-        if (StringUtils.isBlank(configHash) && StringUtils.isNotBlank(configStr)) {
-            configHash = DigestUtils.md5DigestAsHex(configStr.getBytes(StandardCharsets.UTF_8));
+        if (StringUtils.isBlank(configHash) && StringUtils.isNotBlank(pipeLineConfigStr)) {
+            configHash = DigestUtils.md5DigestAsHex(pipeLineConfigStr.getBytes(StandardCharsets.UTF_8));
         }
         return configHash;
     }

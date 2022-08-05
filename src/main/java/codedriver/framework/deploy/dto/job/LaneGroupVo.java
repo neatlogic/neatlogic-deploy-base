15f1b2648f1cdd86ce75637eb060e784d0295053
lvzk <lvzk@techsure.com.cn>
2022-08-05 17:13:28 +0800

    batch deploy job

--- src/main/java/codedriver/framework/deploy/dto/job/LaneGroupVo.java
+++ src/main/java/codedriver/framework/deploy/dto/job/LaneGroupVo.java
@@ -19,12 +19,16 @@ public class LaneGroupVo {
     private Long laneId;
     @EntityField(name = "是否需要等待", type = ApiParamType.INTEGER)
     private Integer needWait = 0;
+    @EntityField(name = "执行完当前组是否停止不继续执行后续组，但仍受needWait约束，1：是，0：否", type = ApiParamType.INTEGER)
+    private Integer isGoon = 1;
     @EntityField(name = "状态", type = ApiParamType.ENUM, member = JobStatus.class)
     private String status;
     @EntityField(name = "作业列表", type = ApiParamType.JSONARRAY)
     private List<DeployJobVo> jobList;
-    @EntityField(name = "重跑类型：   重置并重跑所有：refireResetAll；重跑所有：refireAll", type = ApiParamType.STRING)
-    private String refireType;
+    @EntityField(name = "批量作业执行策略，refire：跳过所有已完成的子作业；reset_refire:执行所有子作业", type = ApiParamType.STRING)
+    private String batchJobAction;
+    @EntityField(name = "子作业执行策略，refire：跳过所有已完成、已忽略的节点；reset_refire:执行所有节点", type = ApiParamType.STRING)
+    private String jobAction;
     @EntityField(name = "排序", type = ApiParamType.INTEGER)
     private Integer sort;
 
@@ -79,11 +83,27 @@ public class LaneGroupVo {
         this.status = status;
     }
 
-    public String getRefireType() {
-        return refireType;
+    public String getBatchJobAction() {
+        return batchJobAction;
     }
 
-    public void setRefireType(String refireType) {
-        this.refireType = refireType;
+    public void setBatchJobAction(String batchJobAction) {
+        this.batchJobAction = batchJobAction;
+    }
+
+    public String getJobAction() {
+        return jobAction;
+    }
+
+    public void setJobAction(String jobAction) {
+        this.jobAction = jobAction;
+    }
+
+    public Integer getIsGoon() {
+        return isGoon;
+    }
+
+    public void setIsGoon(Integer isGoon) {
+        this.isGoon = isGoon;
     }
 }

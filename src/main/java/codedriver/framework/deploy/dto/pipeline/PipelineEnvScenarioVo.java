08fa3a08b62440b8ad2b22fc5e277cac062b35f8
陈其炜 <chenqw@techsure.com.cn>
2022-08-22 11:28:25 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/framework/deploy/dto/pipeline/PipelineEnvScenarioVo.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.deploy.dto.pipeline;
+
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+
+public class PipelineEnvScenarioVo {
+    @EntityField(name = "环境id", type = ApiParamType.LONG)
+    private Long envId;
+    @EntityField(name = "环境名称", type = ApiParamType.STRING)
+    private String envName;
+    @EntityField(name = "场景id", type = ApiParamType.LONG)
+    private Long scenarioId;
+    @EntityField(name = "场景名称", type = ApiParamType.STRING)
+    private String scenarioName;
+
+    public Long getEnvId() {
+        return envId;
+    }
+
+    public void setEnvId(Long envId) {
+        this.envId = envId;
+    }
+
+    public String getEnvName() {
+        return envName;
+    }
+
+    public void setEnvName(String envName) {
+        this.envName = envName;
+    }
+
+    public Long getScenarioId() {
+        return scenarioId;
+    }
+
+    public void setScenarioId(Long scenarioId) {
+        this.scenarioId = scenarioId;
+    }
+
+    public String getScenarioName() {
+        return scenarioName;
+    }
+
+    public void setScenarioName(String scenarioName) {
+        this.scenarioName = scenarioName;
+    }
+}

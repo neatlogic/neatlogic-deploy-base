7fe5b935444ab9cd6b8114ecb559e2ff5b94301d
陈其炜 <chenqw@techsure.com.cn>
2022-08-25 11:45:29 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/framework/deploy/dto/app/DeployProfileVo.java
+++ src/main/java/codedriver/framework/deploy/dto/app/DeployProfileVo.java
@@ -1,14 +1,15 @@
 /*
- * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.framework.deploy.dto.app;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DeployProfileVo {
+public class DeployProfileVo implements Serializable {
 
     private Long profileId;
     private String profileName;

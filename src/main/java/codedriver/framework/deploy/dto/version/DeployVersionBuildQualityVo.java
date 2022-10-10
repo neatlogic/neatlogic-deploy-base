00b72369b08b4100d7026d634caec3377b5bf1cb
赖文韬 <laiwt@techsure.com.cn>
2022-10-10 15:18:02 +0800

    --story=1006924 --user=赖文韬 自动发布-版本中心-新增代码扫描tab https://www.tapd.cn/54247054/s/1255563

--- src/main/java/codedriver/framework/deploy/dto/version/DeployVersionBuildQualityVo.java
+++ src/main/java/codedriver/framework/deploy/dto/version/DeployVersionBuildQualityVo.java
@@ -2,13 +2,14 @@ package codedriver.framework.deploy.dto.version;
 
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.EntityField;
+import codedriver.framework.util.SnowflakeUtil;
 
 import java.util.Date;
 
 public class DeployVersionBuildQualityVo {
 
-    @EntityField(name = "应用模块ID", type = ApiParamType.LONG)
-    private Long appModuleId;
+    @EntityField(name = "id", type = ApiParamType.LONG)
+    private Long id;
     @EntityField(name = "版本ID", type = ApiParamType.LONG)
     private Long versionId;
     @EntityField(name = "构建时间", type = ApiParamType.LONG)
@@ -98,87 +99,18 @@ public class DeployVersionBuildQualityVo {
     private String alertStatus;
     private String qualityGateDetails;
     private String newMaintainabilityRating;
-
-    @EntityField(name = "单元测试总数", type = ApiParamType.INTEGER)
-    private Integer tests;
-    @EntityField(name = "单元测试成功率", type = ApiParamType.INTEGER)
-    private Integer testSuccessDensity;
-    @EntityField(name = "单元测试失败数", type = ApiParamType.INTEGER)
-    private Integer testErrors;
-    @EntityField(name = "全量代码分支覆盖率", type = ApiParamType.INTEGER)
-    private Integer branchCoverage;
-    @EntityField(name = "增量代码分支覆盖率", type = ApiParamType.INTEGER)
-    private Integer newBranchCoverage;
-    @EntityField(name = "全量行覆盖率", type = ApiParamType.INTEGER)
-    private Integer lineCoverage;
-    @EntityField(name = "增量行覆盖率", type = ApiParamType.INTEGER)
-    private Integer newLineCoverage;
     @EntityField(name = "漏洞阈值", type = ApiParamType.INTEGER)
     private Integer threshold;
 
-
-    public Integer getTests() {
-        return tests;
+    public Long getId() {
+        if (id == null) {
+            id = SnowflakeUtil.uniqueLong();
+        }
+        return id;
     }
 
-    public void setTests(Integer tests) {
-        this.tests = tests;
-    }
-
-    public Integer getTestSuccessDensity() {
-        return testSuccessDensity;
-    }
-
-    public void setTestSuccessDensity(Integer testSuccessDensity) {
-        this.testSuccessDensity = testSuccessDensity;
-    }
-
-    public Integer getTestErrors() {
-        return testErrors;
-    }
-
-    public void setTestErrors(Integer testErrors) {
-        this.testErrors = testErrors;
-    }
-
-    public Integer getBranchCoverage() {
-        return branchCoverage;
-    }
-
-    public void setBranchCoverage(Integer branchCoverage) {
-        this.branchCoverage = branchCoverage;
-    }
-
-    public Integer getNewBranchCoverage() {
-        return newBranchCoverage;
-    }
-
-    public void setNewBranchCoverage(Integer newBranchCoverage) {
-        this.newBranchCoverage = newBranchCoverage;
-    }
-
-    public Integer getLineCoverage() {
-        return lineCoverage;
-    }
-
-    public void setLineCoverage(Integer lineCoverage) {
-        this.lineCoverage = lineCoverage;
-    }
-
-    public Integer getNewLineCoverage() {
-        return newLineCoverage;
-    }
-
-    public void setNewLineCoverage(Integer newLineCoverage) {
-        this.newLineCoverage = newLineCoverage;
-    }
-
-    public Long getAppModuleId() {
-        return appModuleId;
-    }
-
-    public void setAppModuleId(Long appModuleId) {
-        this.appModuleId = appModuleId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getVersionId() {

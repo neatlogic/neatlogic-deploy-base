66a7dfef7d533be4c166126b6a85722cfce8140d
chenqw <zenkiray@live.com>
2023-07-28 10:44:55 +0800

    - [修复]响应国家号召修改错别字
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/framework/deploy/dto/app/DeployAppConfigEnvDBConfigVo.java
+++ src/main/java/neatlogic/framework/deploy/dto/app/DeployAppConfigEnvDBConfigVo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package neatlogic.framework.deploy.dto.app;
 
 import neatlogic.framework.common.constvalue.ApiParamType;
@@ -27,9 +43,9 @@ public class DeployAppConfigEnvDBConfigVo {
     private String dbSchema;
     @EntityField(name = "数据库资产id", type = ApiParamType.LONG)
     private Long dbResourceId;
-    @EntityField(name = "帐号id", type = ApiParamType.LONG)
+    @EntityField(name = "账号id", type = ApiParamType.LONG)
     private Long accountId;
-    @EntityField(name = "帐号", type = ApiParamType.STRING)
+    @EntityField(name = "账号", type = ApiParamType.STRING)
     private String account;
     @EntityField(name = "数据库名称", type = ApiParamType.STRING)
     private String dbResourceName;

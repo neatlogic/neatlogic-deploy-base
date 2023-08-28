9cc66b1b66b85b90b716697a1ac7655eccff3497
lvzk <897706680@qq.com>
2023-08-28 14:55:35 +0800

    [功能]保存发布版本用户表头配置 #[958609347805184]自动发布--版本管理页面修改汇总--版本列表添加统计信息字段 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/958609347805184

---
+++ src/main/java/neatlogic/framework/deploy/dto/version/DeployVersionTheadVo.java
@@ -0,0 +1,58 @@
+package neatlogic.framework.deploy.dto.version;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import neatlogic.framework.asynchronization.threadlocal.UserContext;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.restful.annotation.EntityField;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class DeployVersionTheadVo {
+    @EntityField(name = "common.versionid", type = ApiParamType.LONG)
+    private Long versionId;
+
+    @EntityField(name = "common.config", type = ApiParamType.JSONOBJECT)
+    private JSONObject config;
+
+    @JSONField(serialize = false)
+    private String configStr;
+
+    public Long getVersionId() {
+        return versionId;
+    }
+
+    public void setVersionId(Long versionId) {
+        this.versionId = versionId;
+    }
+
+    public JSONObject getConfig() {
+        if(MapUtils.isEmpty(config) && StringUtils.isNotBlank(configStr)) {
+            return JSONObject.parseObject(configStr);
+        }
+        return config;
+    }
+
+    public void setConfig(JSONObject config) {
+        this.config = config;
+    }
+
+    public String getConfigStr() {
+        if(StringUtils.isBlank(configStr) && MapUtils.isNotEmpty(config)) {
+            return config.toString();
+        }
+        return configStr;
+    }
+
+    public void setConfigStr(String configStr) {
+        this.configStr = configStr;
+    }
+
+    public String getUserUuid() {
+        if (UserContext.get() != null) {
+            return UserContext.get().getUserUuid();
+        } else {
+            return null;
+        }
+    }
+}

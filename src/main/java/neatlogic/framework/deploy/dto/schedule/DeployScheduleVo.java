c9627d156a1f89ede7957aa877d6b0b0394c087c
linbangquan <1437892690@qq.com>
2025-06-12 02:03:48 +0000

    !15 [功能] 定时作业管理页增加一列显示作业是否已加载 * [功能] 定时作业管理页增加一列显示作业是否已加载

--- src/main/java/neatlogic/framework/deploy/dto/schedule/DeployScheduleVo.java
+++ src/main/java/neatlogic/framework/deploy/dto/schedule/DeployScheduleVo.java
@@ -21,6 +21,7 @@ import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.common.dto.BaseEditorVo;
 import neatlogic.framework.deploy.constvalue.ScheduleType;
 import neatlogic.framework.restful.annotation.EntityField;
+import neatlogic.framework.scheduler.dto.JobStatusVo;
 import neatlogic.framework.util.SnowflakeUtil;
 import org.apache.commons.lang3.StringUtils;
 
@@ -75,6 +76,10 @@ public class DeployScheduleVo extends BaseEditorVo {
     private Integer editable = 0;
     @EntityField(name = "是否可删除", type = ApiParamType.INTEGER)
     private Integer deletable = 0;
+    @EntityField(name = "是否已加载", type = ApiParamType.INTEGER)
+    private Integer isLoad;
+
+    private JobStatusVo jobStatus;
 
     public Long getId() {
         if (id == null) {
@@ -276,5 +281,21 @@ public class DeployScheduleVo extends BaseEditorVo {
 
     public void setDeletable(Integer deletable) {
         this.deletable = deletable;
+    }
+
+    public Integer getIsLoad() {
+        return isLoad;
+    }
+
+    public void setIsLoad(Integer isLoad) {
+        this.isLoad = isLoad;
+    }
+
+    public JobStatusVo getJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(JobStatusVo jobStatus) {
+        this.jobStatus = jobStatus;
     }
 }
